5efc1e4ad2df7070677e5855999d6ad886eb6f64
David Lemayian <david@code4kenya.org>
2012-11-26 13:52:15 +0300

    Change show on school 2011 results.

--- application/views/results/school_view.php
+++ application/views/results/school_view.php
@@ -16,8 +16,11 @@
 		</div>
 	</div>
 	
-	<h3><?php echo $school_name; ?></h3>
-	<div class="row">
+	<h3><?php echo $school_name; ?> <small>County: <a href="#">County Name</a></small></h3>
+	
+	<br />
+	
+	<div class="row school-results">
 		<div class="span6">
 			<div class="well">
 				<h4>KCPE Results 2010</h4>
@@ -46,20 +49,104 @@
 				<h4>KCPE Results 2011</h4>
 				<hr>
 				<p><b>KNEC Code: </b> <?php echo $res_2011[0]['CODE'] ?></p>
-				<p><b>Students: </b> <?php echo $res_2011[0]['ENTRY'] ?></p>
-				<p><b>National Rank: </b> <?php echo $res_2011[0]['NATIONAL'] ?></p>
+				<p><b>Students: </b> <?php echo $res_2011[0]['ENTRY'] ?>
+					<?php 
+						if ($res_2011[0]['ENTRY']>$res_2010[0]['ENTRY']){
+							echo ' <img src="'.base_url().'assets/img/noun/arrow-up.png" alt="" />';
+						}
+						if ($res_2011[0]['ENTRY']==$res_2010[0]['ENTRY']){
+							echo ' - ';
+						}
+						if ($res_2011[0]['ENTRY']<$res_2010[0]['ENTRY']){
+							echo ' <img src="'.base_url().'assets/img/noun/arrow-down.png" alt="" />';
+						}
+					?>
+				</p>
+				<p><b>National Rank: </b> <?php echo $res_2011[0]['NATIONAL'] ?>
+					<?php 
+						if ($res_2011[0]['NATIONAL']<$res_2010[0]['NATIONAL']){
+							echo ' <img src="'.base_url().'assets/img/noun/arrow-up.png" alt="" />';
+						}
+						if ($res_2011[0]['NATIONAL']==$res_2010[0]['NATIONAL']){
+							echo ' - ';
+						}
+						if ($res_2011[0]['NATIONAL']>$res_2010[0]['NATIONAL']){
+							echo ' <img src="'.base_url().'assets/img/noun/arrow-down.png" alt="" />';
+						}
+					?>
+				</p>
 				<hr />
 				<div class="row">
 					<div class="span2">
-						<p><b>English: </b> <?php echo $res_2011[0]['ENGLISH'] ?></p>
-						<p><b>Kiswahili: </b> <?php echo $res_2011[0]['KISWAHILI'] ?></p>
+						<p><b>English: </b> <?php echo $res_2011[0]['ENGLISH'] ?> 
+							<?php 
+								if ($res_2011[0]['ENGLISH']>$res_2010[0]['ENGLISH']){
+									echo ' <img src="'.base_url().'assets/img/noun/arrow-up.png" alt="" />';
+								}
+								if ($res_2011[0]['ENGLISH']==$res_2010[0]['ENGLISH']){
+									echo ' - ';
+								}
+								if ($res_2011[0]['ENGLISH']<$res_2010[0]['ENGLISH']){
+									echo ' <img src="'.base_url().'assets/img/noun/arrow-down.png" alt="" />';
+								}
+							?>
+						</p>
+						<p><b>Kiswahili: </b> <?php echo $res_2011[0]['KISWAHILI'] ?>
+							<?php 
+								if ($res_2011[0]['KISWAHILI']>$res_2010[0]['KISWAHILI']){
+									echo ' <img src="'.base_url().'assets/img/noun/arrow-up.png" alt="" />';
+								}
+								if ($res_2011[0]['KISWAHILI']==$res_2010[0]['KISWAHILI']){
+									echo ' - ';
+								}
+								if ($res_2011[0]['KISWAHILI']<$res_2010[0]['KISWAHILI']){
+									echo ' <img src="'.base_url().'assets/img/noun/arrow-down.png" alt="" />';
+								}
+							?>
+						</p>
 					</div>
 					<div class="span2">
-						<p><b>Maths: </b> <?php echo $res_2011[0]['MATHS'] ?></p>
-						<p><b>Science: </b> <?php echo $res_2011[0]['SCIENCE'] ?></p>
+						<p><b>Maths: </b> <?php echo $res_2011[0]['MATHS'] ?>
+							<?php 
+								if ($res_2011[0]['MATHS']>$res_2010[0]['MATHS']){
+									echo ' <img src="'.base_url().'assets/img/noun/arrow-up.png" alt="" />';
+								}
+								if ($res_2011[0]['MATHS']==$res_2010[0]['MATHS']){
+									echo ' - ';
+								}
+								if ($res_2011[0]['MATHS']<$res_2010[0]['MATHS']){
+									echo ' <img src="'.base_url().'assets/img/noun/arrow-down.png" alt="" />';
+								}
+							?>
+						</p>
+						<p><b>Science: </b> <?php echo $res_2011[0]['SCIENCE'] ?>
+							<?php 
+								if ($res_2011[0]['SCIENCE']>$res_2010[0]['SCIENCE']){
+									echo ' <img src="'.base_url().'assets/img/noun/arrow-up.png" alt="" />';
+								}
+								if ($res_2011[0]['SCIENCE']==$res_2010[0]['SCIENCE']){
+									echo ' - ';
+								}
+								if ($res_2011[0]['SCIENCE']<$res_2010[0]['SCIENCE']){
+									echo ' <img src="'.base_url().'assets/img/noun/arrow-down.png" alt="" />';
+								}
+							?>
+						</p>
 					</div>
-					<div class="span1" style="text-align: center;">
-						<h5 style="margin-top: 0;">Mean<br /><?php echo $res_2011[0]['MEAN']?></h5>
+					<div class="span1" style="text-align: center; width: 80px;">
+						<h5 style="margin-top: 0;">Mean<br /><?php echo $res_2011[0]['MEAN']?>
+							<?php 
+								if ($res_2011[0]['MEAN']>$res_2010[0]['MEAN']){
+									echo ' <img src="'.base_url().'assets/img/noun/arrow-up.png" alt="" />';
+								}
+								if ($res_2011[0]['MEAN']==$res_2010[0]['MEAN']){
+									echo ' - ';
+								}
+								if ($res_2011[0]['MEAN']<$res_2010[0]['MEAN']){
+									echo ' <img src="'.base_url().'assets/img/noun/arrow-down.png" alt="" />';
+								}
+							?>
+						</h5>
 					</div>
 				</div>
 			</div>
