0e100ad887d718de8cb3131148e6733bb6b26ceb
David Lemayian <david@on.co.ke>
2013-06-14 13:37:53 +0200

    Replaced with Laravel.

--- application/views/templates/redirecting_view.php
+++
@@ -1,7 +0,0 @@
-<section class="container" style="padding:30px 0 0;">
-	<div class="alert alert-success">
-		<p style="color: #999; margin-bottom: 0px;">
-		<img src="<?php echo base_url(); ?>assets/img/spinner.gif" alt="Spinner" class="pull-right" style="padding-top: 3px;"/>
-		<b>Redirecting you...</b> This may take a few moments.</p>
-	</div>
-</section>
