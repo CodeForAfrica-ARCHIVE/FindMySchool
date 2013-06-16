a822f2e09d32372ad964d7f63f48da6a3c643abd
David Lemayian <david@on.co.ke>
2013-06-16 16:54:33 +0300

    Added default and home view.

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -2,4 +2,277 @@
 
 @section('content')
     
+    <?php
+    
+    	/* HOME VIEW
+    	   =========
+    	   /app/views/home.blade.php  */
+    	
+    ?>
+    
+    <!-- LATEST HEADLINE SECTION -->
+    
+    <section style="background: #fff url('/assets/img/bg/home-header.jpg') no-repeat center top; background-size: cover; padding-top: 13px;">
+    	<div class="container" style="">
+    		<div class="alert alert-info">
+    			<button type="button" class="close" data-dismiss="alert">×</button>
+    			<span style="font-weight: bold;">Latest Headlines: {</span>
+    				<a href="#" target="_blank" id="latest-headline-top">School Board on row over teacher pay. [Daily Nation]</a>
+    			<span style="font-weight: bold;">} <a href="#"><i class="icon-chevron-left" style="padding: 3px;"></i></a> <a href="#"><i class="icon-chevron-right"></i></a></span>
+    		</div>
+    		<div class="home-learn-more" >
+    			<img src="/assets/img/air-home.png" alt="" />
+    		</div>
+    	</div>
+    </section>
+    
+    
+    <!-- SEARCH & BROWSE SECTION -->
+    
+    <section style="background-color: #333; text-align: center; color: #fff;">
+    	<div class="container" style="padding: 30px 0 40px;">
+    		<h1 style="margin-bottom: 0;">School Results</h1>
+    		<h2 style="margin-top: 0; line-height: 15px;"><small>Primary &amp; Secondary</small></h2>
+    		
+    		
+    		<div class="row">
+    			<div class="span6" style="text-align: left;">
+    				<div class="page-header" style="margin-bottom: 15px;">
+    					<h3 style="margin-bottom: 0;">Top Primary Schools 2011</h3>
+    				</div>
+    				<div class="top-schools" id="pri_results">
+    					<ol>
+    						<?php
+//    							foreach ($top_kcpe as $school) {
+//    								echo '<li><a href="'.base_url().'results/school/pri:'.$school['CODE'].'">'.
+//    									ucwords(strtolower($school['SCHOOL NAME'])).'</a></li>';
+//    							}
+    						?>
+    					</ol>
+    				</div>
+    			</div>
+    			<div class="span6" style="text-align: right;">
+    				<div>
+    					<h3>Search Schools Database</h3>
+    					<div class="input-append" style="margin: 10px auto; display: inline-block;">
+    						<input type="text" class="span3" id="search_term" placeholder="School Name" onkeypress="return runScript(event)"  style="font-size: 18px; line-height: 25px; height: 22px; padding: 10px 10px 10px 15px;">
+    						<button class="btn btn-large btn-purple" id="search_results_btn" type="button"><i class="icon-search"></i> Search</button>
+    					</div>
+    					<p>example: <a href="#" class="search-example">Alliance Secondary School</a></p>
+    				</div>
+    				<div class="clearfix"></div>
+    				
+    				<hr style="border-top: none;" />
+    				
+    				<div class="home-school-results-btns">
+    					<p><a href="#" class="btn btn-large btn-purple"><i class="icon-list-alt"></i> Browse Secondary Schools</a></p>
+    					<p><a href="#" class="btn btn-large btn-purple"><i class="icon-map-marker"></i> Discover Schools Near You</a></p>
+    					<p><a href="#" class="btn btn-large btn-purple"><i class="icon-info-sign"></i> About Find My School</a></p>
+    				</div>
+    				
+    			</div>
+    		</div>
+    	</div>
+    </section>
+    
+    
+    <!-- DISCOVER SECTION -->
+    
+    <section style="background: #fff url('/assets/img/bg/nearme.png') repeat-x center top; padding-bottom: 30px;">
+    
+    	<div class="container">
+    		<div class="page-header" style="border-bottom: 0; padding-bottom: 0px; padding-top: 9px; margin: 20px 0 30px; border-top: 1px solid #eee; text-align: center;">
+    			<h2>Discover Schools Near You</h2>
+    		</div>
+    		
+    		<p class="lead" style="text-align: center;">Find schools near you using our <b>interactive map</b>. Simply drag the marker on the map below and hit "<b>Discover Schools</b>".
+    			Also make sure to <b>zoom in and out</b> to get a better sense of loaction.</p>
+    		
+    		<div style="background: #fff url('/assets/img/bg/shadow-960-up.png') no-repeat center top; padding-top: 4px;">
+    			<div id="map_canvas" style="height: 300px; width: 100%;">
+    				<table style="height: 100%; width: 100%; vertical-align: middle; text-align: center;" >
+    					<tbody><tr><td>
+    						<p><img src="/assets/img/spinner.gif" alt="" /> Loading map...</p>
+    					</td></tr></tbody>
+    				</table>
+    			</div>
+    		</div>
+    		<div style="background: #fff url('/assets/img/bg/shadow-960.png') no-repeat center top; padding-top: 4px;"></div>
+    		
+    		<p style="text-align: center; margin-top: 20px;">
+    			<a href="javascript:setDiscLocation();" class="btn btn-large btn-purple">
+    				<i class="icon-globe" style="visibility: hidden;"></i>Discover Schools <i class="icon-globe icon-white"></i>
+    		</a></p>
+    	</div>
+    	
+    </section>
+    
+    <hr class="container"/>
+    
+    <!-- OTHER ITEMS SECTION -->
+    <section class="container">
+    	<div class="row">
+    		<div class="span7">
+    			<div class="page-header">
+    				<h3>About Find My School <span style="font-size: 20px;">.Ke</span></h3>
+    			</div>
+    			<p style="text-indent: 40px;">Ut mattis risus in elit fermentum nec lobortis nisl ultricies. Aliquam ligula ante, eleifend vitae euismod et, sodales et ligula. In accumsan ipsum a orci fermentum quis luctus libero sagittis. In imperdiet sem vel turpis varius in congue lectus iaculis. Duis quis venenatis leo. Suspendisse potenti. Integer tristique odio vel nisi lobortis pulvinar. Pellentesque commodo blandit nisi, a eleifend felis convallis sit amet. Etiam justo nunc, facilisis eget fringilla a, scelerisque ultricies risus.</p>
+    			<br />
+    			<p class="about-logos" style="">
+    				<a href="http://www.knec.ac.ke" target="_blank" style="padding: 0 10px; border-right: 1px solid #777;">
+    					<img src="/assets/img/logos/knec.jpg" alt="KNEC" style="height: 70px;" />
+    				</a>
+    				<a href="http://twaweza.org" target="_blank" style="padding: 0 10px; border-right: 1px solid #777;">
+    					<img src="/assets/img/logos/twaweza.png" alt="Twaweza" style="height: 80px;" />
+    				</a>
+    				<a href="http://code4kenya.org" target="_blank" style="padding: 0 10px;">
+    					<img src="/assets/img/logos/c4k_2.png" alt="Code4Kenya" style="height: 50px;" />
+    				</a>
+    			</p>
+    		</div>
+    		<div class="span5">
+    			<div class="page-header">
+    				<h3>From Our Blog 
+    					<small><a href="#"><i class="icon-link"></i> - blog.findmyschool.co.ke</a></small>
+    				</h3>
+    			</div>
+    			<ul class="media-list">
+    				<?php foreach (array(0,1,2) as $visualization) { ?>
+    					<li class="media">
+    						<a class="pull-left" href="#">
+    							<img class="media-object" data-src="/assets/js/vendor/holder.js/64x64">
+    						</a>
+    						<div class="media-body">
+    							<h4 class="media-heading">Media heading</h4>
+    							<p>...</p>
+    						</div>
+    					</li>
+    				<?php } ?>
+    			</ul>
+    			<p style="text-align: right;">
+    				
+    			</p>
+    		</div>
+    	</div>
+    </section>
+    
+    
+    
+    <!-- JS Scripts for this Page -->
+    
+    <!-- BY SEARCH -->
+    
+    <script type="text/javascript">
+    	
+    	// Actual Search
+    	var search_term;
+    	var search_schools_btn = document.getElementById("search_results_btn");
+    	search_schools_btn.onclick = function() {
+    		search_schools_btn.innerHTML="<i class=\"icon-search icon-white\"></i> Please wait...";
+    		search_term = document.getElementById("search_term").value;
+    		window.location.href = "/results/search/"+search_term;
+    	}
+    	
+    	function runScript(e) {
+    	    if (e.keyCode == 13) {
+    	    	search_schools_btn.innerHTML="<i class=\"icon-search icon-white\"></i> Please wait...";
+    	        search_term = document.getElementById("search_term").value;
+    	        window.location.href = "/results/search/"+search_term;
+    	        return false;
+    	    }
+    	}
+    	
+    </script>
+    
+    <!-- BY MARKS -->
+    <script type="text/javascript">
+    	var county_id = $('#county_select option:selected').val();
+    	var county_name = encodeURIComponent($('#county_select option:selected').text());
+    	var marks_in = $('#appendedInput').val();
+    	
+    	function run_engine() {
+    		county_id = $('#county_select option:selected').val();
+    		county_name = encodeURIComponent($('#county_select option:selected').text());
+    		marks_in = $('#appendedInput').val();
+    		
+    		if (county_id == 0) {
+    			$('#county_ctrl_group').addClass('control-group error');
+    		} else {
+    			$('#county_ctrl_group').removeClass('control-group error');
+    		}
+    		if (marks_in == "" || !$.isNumeric(marks_in)) {
+    			$('#marks_ctrl_group').addClass('control-group error');
+    		} else {
+    			$('#marks_ctrl_group').removeClass('control-group error');
+    		}
+    		
+    		if (county_id != 0 && $.isNumeric(marks_in)){
+    			if (marks_in>0 && marks_in<500) {
+    				window.location.href = "/marks/compare/"+county_name+":"+county_id+":"+marks_in;
+    			} else {
+    				$('#marks_ctrl_group').addClass('control-group error');
+    			}
+    		}
+    		
+    	}
+    </script>
+    
+    
+    <!-- BY LOCATION -->
+    
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?AIzaSyDl0_EPlseIlbNlYZDOzpua7VqXyH_LfeY&sensor=true"></script>
+    <script type="text/javascript">
+    
+    var disc_lat = -1.29353;
+    var disc_long = 36.819889;
+    
+    var nairobi = new google.maps.LatLng(-1.29353, 36.819889);
+    var marker;
+    var map;
+    
+    var mapOptions = {
+    	zoom: 11,
+    	mapTypeId: google.maps.MapTypeId.ROADMAP,
+    	center: nairobi
+    };
+    
+    window.onload = function(){	
+    	
+    	map = new google.maps.Map(document.getElementById("map_canvas"),
+    		mapOptions);
+    	
+    	marker = new google.maps.Marker({
+    		map:map,
+    		draggable:true,
+    		animation: google.maps.Animation.DROP,
+    		position: nairobi
+    	});
+    	
+    	google.maps.event.addListener(marker, 'click', toggleBounce);
+    	//google.maps.event.addListener(marker, 'dragend', setDiscLocation);
+    	
+    	function toggleBounce() {
+    		if (marker.getAnimation() != null) {
+    			marker.setAnimation(null);
+    		} else {
+    			marker.setAnimation(google.maps.Animation.BOUNCE);
+    		}
+    	}
+    	
+    }
+    
+    function setDiscLocation() {
+    	var point = marker.getPosition();
+    	disc_lat = point.lat();
+    	disc_long = point.lng();
+    	window.location.href = "/discover/locate/"+disc_lat+":"+disc_long;
+    }
+    	
+    </script>
+    
+    
+    <!-- AddThis Script -->
+    <script type="text/javascript" src="http://s7.addthis.com/js/300/addthis_widget.js#pubid=xa-508d41213e72f77b"></script>
+    
+    
 @stop
--- app/views/layout/default.blade.php
+++ app/views/layout/default.blade.php
@@ -21,23 +21,169 @@
 		<!--[if IE 7]>
 			<link rel="stylesheet" href="/assets/css/font-awesome-ie7.min.css">
 		<![endif]-->
-		<script src="/assets/js/vendor/jquery-1.9.1.min.js"></script>
+		<link rel="stylesheet" href="/assets/css/style.css">
+		
+		<script src="/assets/js/vendor/jquery-1.10.1.min.js"></script>
 		
 	</head>
 	
 	<body>
+		
 		<!--[if lt IE 7]>
 			<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
 		<![endif]-->
 		
-		<!-- Add your site or application content here -->
+		<header>
+			<div class="navbar navbar-fixed-top">
+				<div class="beta">
+					<p>Under Construction: We are in <span>BETA</span> <img src="/assets/img/noun/construction.png" alt=""/></p>
+				</div>
+				<div class="navbar-inner">
+					<div class="container" style="margin-top: 5px;">
+						<!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+						<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</a>
+						
+						<!-- Be sure to leave the brand out there if you want it shown -->
+						<a class="brand" href="/" style="margin-top: 10px;">
+							<img src="/assets/img/icon.png" style="height: 30px; margin-top: -15px; padding-right: 5px;"/>
+							Find My School <small>.Ke</small>
+						</a>
+						
+						<!-- Everything you want hidden at 940px or less, place within here -->
+						<div class="nav-collapse pull-right">
+							<ul class="nav">
+								<!--<li style="padding-top: 7px;"><a href="/">Home</a></li>-->
+								<li class="divider-vertical"></li>
+								<li><a href="/discover">Discover<br/><span>Schools Near You</span></a></li>
+								<!--<li class="divider-vertical"></li>
+								<li><a href="/marks">Marks Comparison<br/><span>For Secondary School Placement</span></a></li>-->
+								<li class="divider-vertical"></li>
+								<li><a href="/results">Performance<br/><span>KCPE and KCSE Results</span></a></li>
+								<li class="divider-vertical"></li>
+								<li><a href="/about">About<br/><span>Find My School</span></a></li>
+							</ul>
+						</div>
+					
+					</div>
+				</div>
+			</div>
+		</header>
+		        
+        <article style="margin-top: 87px;">
+        
+			@yield('content')
+			
+		</article>
 		
+		<!-- Footer stuff -->
 		
+		<footer>
+			
+			<section class="container">
+				<div class="row">
+				
+					<div class="span4">
+						<h3 class="footer-title">Recent Visualizations</h3>
+						<ul class="media-list">
+							<?php foreach (array(0,1,2,3) as $visualization) { ?>
+								<li class="media">
+									<a class="pull-left" href="#">
+										<img class="media-object" data-src="/assets/js/vendor/holder.js/64x64">
+									</a>
+									<div class="media-body">
+										<h4 class="media-heading">Media heading</h4>
+										<p>...</p>
+									</div>
+								</li>
+							<?php } ?>
+						</ul>
+					</div>
+					
+					<div class="span4">
+						<h3 class="footer-title">Latest Headlines</h3>
+						<div id="latest-headlines-stream">
+							<!-- Latest Headlines Stream -->
+						</div>
+					</div>
+					
+					<div class="span4">
+						<div>
+							<h3 class="footer-title" >Latest Tweets 
+								<a href="https://twitter.com/FindMySchool" class="twitter-follow-button" data-show-count="false">@FindMySchool</a>
+							</h3>
+							<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+						</div>
+						
+						<ul id="latest-tweets-stream">
+							
+						</ul>
+						
+					</div>
+				</div>
+				
+			</section>
+			
+			<section class="container footer-links">
+				<hr style="border-top: 0;"/>
+				<p>
+					<a href="/">Home</a> 
+					<a href="#">About</a> 
+					<a href="#">Help</a> - 
+					<a href="#">Blog</a> 
+					<a href="#">Twitter</a> - 
+					<a href="#">API</a> 
+					<a href="#">Terms</a>
+					
+					<a href="http://code4kenya.org" target="_blank" class="pull-right c4k-logo" style="border-left: 1px #333 solid;">
+						<img src="/assets/img/logos/c4k_2.png" alt="Code4Kenya" style="height: 40px;"/>
+					</a>
+					<a href="http://twaweza.org" target="_blank" class="pull-right c4k-logo" style="border-left: 1px #333 solid;">
+						<img src="/assets/img/logos/twaweza.png" alt="Twaweza" style="height: 68px;">
+					</a>
+					<a href="http://www.knec.ac.ke" target="_blank" class="pull-right c4k-logo">
+						<img src="/assets/img/logos/knec.jpg" alt="KNEC" style="height: 60px;">
+					</a>
+				</p>
+			</section>
+			
+		</footer>
 		
-		@yield('content')
+		<!-- SCRIPTS -->
 		
 		<script src="/assets/js/plugins.js"></script>
 		<script src="/assets/js/main.js"></script>
+		<script src="/assets/js/bootstrap.min.js"></script>
+        <script src="/assets/js/vendor/holder.js"></script>
+        
+        <script src="/assets/js/vendor/raphael.min.js"></script>
+		
+		<script src="/assets/js/vendor/jquery.feeds.js"></script>
+        <script type="text/javascript">
+        
+        	// Latest News Feed
+			
+			$('#latest-headlines-stream').feeds({
+				feeds: {
+					feed1: 'https://news.google.com/news/feeds?q=kenya%20education&output=rss'
+				},
+				max: 3
+			});
+			
+			// Latest Twitter Feed
+			
+			$('#latest-tweets-stream').feeds({
+				feeds: {
+					feed1: 'https://api.twitter.com/1/statuses/user_timeline/openinstitute.xml?count=5&include_rts=1'
+				},
+				xml: true,
+				max: 5
+			});
+			
+        </script>
 		
 		<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
 		<script>
@@ -46,5 +192,6 @@
 			g.src='//www.google-analytics.com/ga.js';
 			s.parentNode.insertBefore(g,s)}(document,'script'));
 		</script>
+		
 	</body>
 </html>
